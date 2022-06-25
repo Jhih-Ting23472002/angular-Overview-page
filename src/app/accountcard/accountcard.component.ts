fba0c8abff16437c139514d2e41a88d44e628bea
JHIH-TING <tina23472002@gmail.com>
2022-06-25 13:44:30 +0800

    切版

---
+++ src/app/accountcard/accountcard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accountcard',
+  templateUrl: './accountcard.component.html',
+  styleUrls: ['./accountcard.component.css']
+})
+export class AccountcardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { CardComponent } from './card/card.component';
+import { AccountcardComponent } from './accountcard/accountcard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BoardComponent,
+    CardComponent,
+    AccountcardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
