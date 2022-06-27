343e6d245f68ecb76e7093fae8a6bde3f1b68e1e
JHIH-TING <tina23472002@gmail.com>
2022-06-27 18:04:20 +0800

    部分點擊

--- src/app/accountcard/accountcard.component.ts
+++ src/app/accountcard/accountcard.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountcardComponent implements OnInit {
 
+  show: boolean = false;//眼睛開關
+  noShow: boolean = true; //眼睛開關
+  // green: boolean = false;
+  // green2: boolean = false;
+
+  days:string = '14'; //天數
+  transform:boolean = false; //按鈕切換css控制
+
   constructor() { }
 
+
+  filterData($event: any){
+    const days = ($event.currentTarget.childNodes)[1].innerHTML;
+    this.days = days;
+
+
+    if(days === '30'){
+      this.transform = true;
+    }else {
+      this.transform = false;
+    }
+  }
+
+
+
   ngOnInit(): void {
+
+
   }
 
 }
